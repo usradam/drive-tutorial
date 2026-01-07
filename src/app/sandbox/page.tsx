2e4dac87a0026e79444c187b7e54f9f9222cc509
Adam <147444558+usradam@users.noreply.github.com>
2026-01-07 13:18:34 -1000

    db model working

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,39 @@
+import { db } from "~/server/db";
+import { mockFolders, mockFiles } from "~/lib/mock-data";
+import { files, folders } from "~/server/db/schema";
+
+export default function SandboxPage() {
+    return (
+        <div className="flex flex-col gap-4">
+            Seed Function
+            <form action={async () => {
+                "use server";
+                
+               const folderInsert = await db.insert(folders).values(
+                    mockFolders.map((folder, index) => ({
+                        id: index + 1,
+                        name: folder.name,
+                        parent: index !== 0 ? 1 : null,
+                    })),
+                );
+
+                console.log(folderInsert);
+
+                const fileInsert = await db.insert(files).values(
+                    mockFiles.map((file, index) => ({
+                        id: index + 1,
+                        name: file.name,
+                        size: 50000,
+                        url: file.url,
+                        parent: (index % 3) + 1,
+                    })),
+                );
+
+                console.log(fileInsert);
+            }}
+            >
+                <button type="submit">Seed</button>
+            </form>
+        </div>
+    );
+}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,33 @@
-import { int, bigint, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, bigint, text, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = singlestoreTableCreator(
+  (name) => `drive_tutorial_${name}`,
+);
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent",{ mode: "number", unsigned: true }).notNull(),
+},
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+},
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
