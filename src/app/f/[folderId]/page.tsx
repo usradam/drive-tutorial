c3cf22a7b45a2d04f0a89e7971bb23aaa6a4da14
Adam <147444558+usradam@users.noreply.github.com>
2026-01-08 19:00:04 -1000

    Cleanup data access pattern

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,25 +1,5 @@
-import { db } from "~/server/db"
-import { files_table as filesSchema, folders_table as foldersSchema } from "~/server/db/schema"
 import DriveContents from "../../drive-contents"
-import { eq } from "drizzle-orm"
-
-async function getAllParents(folderId: number) {
-    const parents = [];
-    let currentId: number | null = folderId;
-    while (currentId !== null) {
-        const folder = await db
-            .selectDistinct()
-            .from(foldersSchema)
-            .where(eq(foldersSchema.id, currentId));
-        
-        if (!folder[0]) {
-            throw new Error("Folder not found");
-        }
-        parents.unshift(folder[0]);
-        currentId = folder[0]?.parent;
-    }
-    return parents;
-}
+import { getAllParentsForFolder, getFolders, getFiles } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
     params: Promise<{ folderId: string }>
@@ -30,29 +10,12 @@ export default async function GoogleDriveClone(props: {
     if (isNaN(parsedFolderId)) {
         return <div>Invalid folder ID</div>;
     }
-
-    const foldersPromise = db
-        .select()
-        .from(foldersSchema)
-        .where(eq(foldersSchema.parent, parsedFolderId));
-    
-    const filesPromise = db
-        .select()
-        .from(filesSchema)
-        .where(eq(filesSchema.parent, parsedFolderId));
-    
-    const parentsPromise = getAllParents(parsedFolderId);
     
     const [folders, files, parents] = await Promise.all([
-        foldersPromise,
-        filesPromise,
-        parentsPromise
+        getFolders(parsedFolderId),
+        getFiles(parsedFolderId),
+        getAllParentsForFolder(parsedFolderId)
     ]);
     
-    return <DriveContents
-        files={files}
-        folders={folders}
-        parents={parents}
-    />
+    return <DriveContents files={files} folders={folders} parents={parents} />
 }
-
